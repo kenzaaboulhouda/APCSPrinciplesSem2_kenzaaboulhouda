45d8c1eb54af59ae6dd4b87c42da7c22effdc628
kenza aboulhouda <796606@seq.org>
2019-01-10 11:32:29 -0800

    select sort

--- Lab110BubbleSelect/bubbleselect.js
+++ Lab110BubbleSelect/bubbleselect.js
@@ -18,5 +18,15 @@ function bubbleSort(Bars){
   }
 }
 }
-
 //selectionSort
+for (var i = 0; i < arr.length - 1; i++)
+        {
+            var index = i;
+            for (var j = i + 1; j < arr.length; j++)
+    if (arr[j] < arr[index])
+       index = j;
+      		  //  Swapping Code
+            var smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
