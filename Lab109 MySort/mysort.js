be0caff309e24f2fb3022862f7c5212d29b9085b
Kenza Aboulhouda <796606@seq.org>
2019-01-10 08:45:38 -0800

    deletion

--- Lab109 MySort/mysort.js
+++ Lab109 MySort/mysort.js
@@ -4,26 +4,3 @@
 **  Oct 29, 2018
 */
 //global variables
-var Bars = [];
-
-function Colorbar(location, width, height, color){
-  // Instance variables
-  this.loc = location;
-  this.w = width;
-  this.h = height;
-  this.clr = color;
-  //this.clrAvg = clrAvg;
-
-  // This function calls other functions
-  this.run = function(){
-    this.render();
-  }
-
-  // render() draws the bars
-  this.render = function(){
-    fill(this.clr);
-    rect(this.loc.x, this.loc.y, this.w, this.h);
-
-  }
-
-}//  End of colorBars Constructor
