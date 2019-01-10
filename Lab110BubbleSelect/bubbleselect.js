d3e936b5c46e2a454e3dbe5289c9fe74e7fd344a
Kenza Aboulhouda <796606@seq.org>
2019-01-10 08:49:23 -0800

    commit

--- Lab110BubbleSelect/bubbleselect.js
+++ Lab110BubbleSelect/bubbleselect.js
@@ -3,4 +3,20 @@
 **  Kenza Aboulhouda
 **  Oct 29, 2018
 */
-//global variables
+//bubbleSort
+function bubbleSort(Bars){
+  var length = Bars.length;
+  for (var i = 0; i < length; i--){
+    for (var j = 0; j < (length-i-1); j++){
+    var a = Bars[j].clrAvg;
+    var b = Bars[j+1].clrAvg;
+    if (a > b){
+      var temp = Bars[j];
+      Bars[j] = Bars[j+1];
+      Bars[j+1] = temp;
+    }
+  }
+}
+}
+
+//selectionSort
