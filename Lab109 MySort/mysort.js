ec4971515d1b8654615794cb515f038f5a9c2323
kenza aboulhouda <796606@seq.org>
2019-01-11 08:01:40 -0800

    Update Lab109 MySort/mysort.js

--- Lab109 MySort/mysort.js
+++ Lab109 MySort/mysort.js
@@ -1,6 +1,15 @@
 /*
-**  ColorBar Constructor Function
+**  MySort Constructor Function
 **  Kenza Aboulhouda
 **  Oct 29, 2018
 */
-//global variables
+//MySort
+for (var i = 0; i < arr.length - 1; i++)
+        {
+            var index = i;
+            for (var j = i + 1; j < arr.length; j++)
+            //  Swapping Code
+            var smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
