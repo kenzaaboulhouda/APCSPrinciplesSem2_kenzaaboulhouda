d8e7b2a681e8da0956e699036798c3d0a364109c
Kenza Aboulhouda <796606@seq.org>
2019-03-05 08:50:29 -0800

    stuff

--- ArtOne227/sketch.js
+++ ArtOne227/sketch.js
@@ -8,8 +8,9 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(203, 227, 239);
   b1= new Ball(createVector(width/2,height/2),
-createVector(.5,-.8),25, color(242, 205, 228));
+createVector(.5,-.8),25, color(255), random(255), random(255));
   loadBalls(150);  //  !!!!!!!!!!  this is a function call
+  frameRate(30)
 }
 
 function draw() {
@@ -26,7 +27,7 @@ function loadBalls(numBalls){
         var loc = createVector(random(width), random(height));
         var vel = createVector(random(-2,4), random(-2,4));
         var rad = random(10,20);
-        var col = color(242, 205, 228);
+        var col = color(random(255), random(255), random(255));
         balls.push(new Ball(loc, vel, rad, col));
     }
     console.log(balls);
